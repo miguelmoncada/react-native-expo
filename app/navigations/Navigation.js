2473fbb4fe06f5c003d328039b01a98a1afdc095
Miguel Moncada <miguelmoncadag@gmail.com>
2020-04-04 21:43:21 -0600

    Menu for Restaurant on the bottom

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -4,31 +4,19 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import RestaurantsScreen from '../screens/Restaurants'
 import MyAccountScreen from '../screens/MyAccount'
 
-
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <Tab.Navigator>
       <Tab.Screen 
-          name="Home" 
-          component={MyAccountScreen}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            )
-          }} 
-      />
-      <Tab.Screen 
-          name="Settings" 
-          component={RestaurantsScreen}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="settings" color={color} size={size} />
-            )
-          }} 
+        name="Restaurants" 
+        component={RestaurantsScreen}
+        options={{      
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="silverware-fork-knife" color={color} size={size} />
+          )
+        }} 
       />
     </Tab.Navigator>
   );
