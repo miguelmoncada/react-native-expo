d62f5abdc74f00bebd12d3a5a2047735a9544615
Miguel Moncada <miguelmoncadag@gmail.com>
2020-04-04 22:59:23 -0600

    Improvements on Navigations Tabs

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import NavigationTabs from './app/navigations/Navigation'
+import Navigation from './app/navigations/Navigation'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NavigationTabs />
-    </NavigationContainer>
+    <Navigation />
   );
 }
--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,23 +1,80 @@
 import React from 'react';
+import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import RestaurantsScreen from '../screens/Restaurants'
+import TopRestaurantsScreen from '../screens/TopRestaurants'
+import SearchScreen from '../screens/Search'
 import MyAccountScreen from '../screens/MyAccount'
 
-const Tab = createBottomTabNavigator();
+import CreateStacks from './CreateStacks'
 
-export default function Navigation() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen 
-        name="Restaurants" 
-        component={RestaurantsScreen}
-        options={{      
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="silverware-fork-knife" color={color} size={size} />
-          )
-        }} 
-      />
-    </Tab.Navigator>
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+function RestaurantsStack() {
+  return( <CreateStacks name="Restaurants" component={RestaurantsScreen} /> );
+}
+
+function TopRestaurantsStack() {
+  return( <CreateStacks name="Top 5 Restaurants" component={TopRestaurantsScreen} /> );
+}
+
+function SearchStack() {
+  return( <CreateStacks name="Search" component={SearchScreen} /> );
+}
+
+function MyAccountStack() {
+  return( <CreateStacks name="My Account" component={MyAccountScreen} /> );
+}
+
+export default function Navigation(){
+  
+
+
+  return(
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Restaurants" 
+          component={RestaurantsStack}
+          options={{      
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="silverware-fork-knife" color={color} size={size} />
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name="Top 5 Restaurants" 
+          component={TopRestaurantsStack}
+          options={{      
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="cup" color={color} size={size} />
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name="Search" 
+          component={SearchStack}
+          options={{      
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="cup" color={color} size={size} />
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name="My Account" 
+          component={MyAccountStack}
+          options={{      
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="cup" color={color} size={size} />
+            )
+          }} 
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
