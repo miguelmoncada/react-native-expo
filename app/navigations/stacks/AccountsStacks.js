4b8a2aebfead4d291783bbcc3fc5a1a3972bdd6b
Miguel Moncada <miguelmoncadag@gmail.com>
2020-04-07 22:49:28 -0600

    Added Register View

--- app/navigations/stacks/AccountsStacks.js
+++ app/navigations/stacks/AccountsStacks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MyAccountScreen from '../../screens/Account/MyAccount'
 import LoginScreen from '../../screens/Account/Login'
+import RegisterScreen from '../../screens/Account/Register'
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export default function AccountsStacks() {
     <Stack.Navigator>
       <Stack.Screen name="My Account" component={MyAccountScreen}/>
       <Stack.Screen name="Login"component={LoginScreen}/>
+      <Stack.Screen name="Register" component={RegisterScreen}/>
     </Stack.Navigator>
   );
 }
--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,10 +1,67 @@
 import React from 'react'
 import { StyleSheet ,Text, View, ScrollView, Image } from 'react-native'
+import { Divider } from 'react-native-elements'
 
 export default function Login(props) {
+    
+    const {navigation} = props 
+
     return (
-        <View>
-            <Text> Formulario Login... </Text>
+      <ScrollView>
+          <Image 
+            source={require("../../../assets/img/logo.png")}
+            style={style.image}
+            resizeMode="contain"
+          />
+        <View style={style.viewContainer}>
+            <CreateAccount navigation={navigation}/>
         </View>
-    )
+        <Divider style={style.divider} />
+        <View style={style.viewContainer}>
+        </View>
+      </ScrollView>   
+    );
 }
+
+function CreateAccount(props) {
+
+    const {navigation} = props 
+
+    return (
+         <Text style={style.textRegister}>
+             ¿Aun no tienes una cuenta?{" "}
+             <Text
+                style={style.btnRegister}
+                onPress={()=> {navigation.navigate("Register")}}
+             >
+                Regístrarte
+             </Text>
+         </Text>
+    );
+}
+
+const style = StyleSheet.create({
+    btnRegister: {
+        color: "#00a680",
+        fontWeight: "bold",
+
+    },
+    textRegister: {
+        marginTop: 15,
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    image: {
+        height:120,
+        width: "100%",
+        marginTop: 20,
+    },
+    viewContainer: {
+        marginRight: 40,
+        marginLeft: 40,
+    },
+    divider: {
+        backgroundColor: "#00a680",
+        margin: 40,
+    }
+});
