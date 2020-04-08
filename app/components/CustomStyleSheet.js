01de98392d0920ff1c23c3304ce8ae8a42cdf208
Miguel Moncada <miguelmoncadag@gmail.com>
2020-04-07 23:52:37 -0600

    Add Colors for Future Themes

---
+++ app/components/CustomStyleSheet.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  theme_color_0: {
+    color: "#00a680"
+  },
+  theme_color_0: {
+    color: "#4267b2"
+  },
+});
